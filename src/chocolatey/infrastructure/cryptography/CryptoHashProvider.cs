c6675e9c86517b1cb36f29c7b1c533a0e3d126c6
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2024-05-31 14:11:31 -0500

    Use DotNet SHA only

--- src/chocolatey/infrastructure/cryptography/CryptoHashProvider.cs
+++ src/chocolatey/infrastructure/cryptography/CryptoHashProvider.cs
@@ -41,17 +41,8 @@ namespace chocolatey.infrastructure.cryptography
 
         private static IHashAlgorithm GetHashAlgorithmStatic(CryptoHashProviderType algorithmType)
         {
-
-            var fipsOnly = false;
-            try
-            {
-                fipsOnly = CryptoConfig.AllowOnlyFipsAlgorithms;
-            }
-            catch (Exception ex)
-            {
-                "chocolatey".Log().Debug("Unable to get FipsPolicy from CryptoConfig:{0} {1}".FormatWith(Environment.NewLine, ex.Message));
-            }
-
+            // As per https://web.archive.org/web/20221109021831/https://learn.microsoft.com/en-us/dotnet/standard/security/cryptography-model#how-algorithms-are-implemented-in-net
+            // The SHA*.create() methods in dotnet core use the OS algorithms, which are FIPS certified on Windows
             HashAlgorithm hashAlgorithm = null;
             switch (algorithmType)
             {
@@ -59,13 +50,13 @@ namespace chocolatey.infrastructure.cryptography
                     hashAlgorithm = new HashAlgorithm(MD5.Create());
                     break;
                 case CryptoHashProviderType.Sha1:
-                    hashAlgorithm = new HashAlgorithm(fipsOnly ? new SHA1Cng() : SHA1.Create());
+                    hashAlgorithm = new HashAlgorithm(SHA1.Create());
                     break;
                 case CryptoHashProviderType.Sha256:
-                    hashAlgorithm = new HashAlgorithm(fipsOnly ? new SHA256Cng() : SHA256.Create());
+                    hashAlgorithm = new HashAlgorithm(SHA256.Create());
                     break;
                 case CryptoHashProviderType.Sha512:
-                    hashAlgorithm = new HashAlgorithm(fipsOnly ? new SHA512Cng() : SHA512.Create());
+                    hashAlgorithm = new HashAlgorithm(SHA512.Create());
                     break;
             }
 
